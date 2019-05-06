190378a28286cfb8c450136b70bcd8404465ec06
Slxm Jxmmi <slxm@localhost.localdomain>
2019-05-06 13:16:39 +0300

    Updated

--- __mocks__/Globals.js
+++ __mocks__/Globals.js
@@ -1,8 +1,8 @@
 const port = require('../app')
 
 module.exports = {
-    facebook: "https://agile-taiga-13487.herokuapp.com:" + port.fbPort + "/facebook/",
-    zeliaSignUp: "https://agile-taiga-13487.herokuapp.com:" + port.zeliaPort + "/zelia/signup",
-    zeliaLogin: "https://agile-taiga-13487.herokuapp.com:" + port.zeliaPort + "/zelia/login",
-    zeliaData: "https://agile-taiga-13487.herokuapp.com:" + port.zeliaPort +"/zelia/data"
+    facebook: "localhost:" + port.fbPort + "/facebook/",
+    zeliaSignUp: "localhost:" + port.zeliaPort + "/zelia/signup",
+    zeliaLogin: "localhost:" + port.zeliaPort + "/zelia/login",
+    zeliaData: "localhost:" + port.zeliaPort +"/zelia/data"
 }
